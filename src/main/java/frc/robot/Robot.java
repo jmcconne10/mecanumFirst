e4a904eb747ff1f37db51eda4c7762141ec6f380
Joe McConnell <joe.mcconnell10@gmail.com>
2023-10-07 19:12:11 -0500

    Changed to Xbox controller

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -8,6 +8,7 @@ import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.drive.MecanumDrive;
 import edu.wpi.first.wpilibj.motorcontrol.PWMSparkMax;
+import edu.wpi.first.wpilibj.XboxController;
 
 /** This is a demo program showing how to use Mecanum control with the MecanumDrive class. */
 public class Robot extends TimedRobot {
@@ -16,10 +17,9 @@ public class Robot extends TimedRobot {
   private static final int kFrontRightChannel = 1;
   private static final int kRearRightChannel = 0;
 
-  private static final int kJoystickChannel = 0;
+  private final XboxController m_driverController = new XboxController(0);
 
   private MecanumDrive m_robotDrive;
-  private Joystick m_stick;
 
   @Override
   public void robotInit() {
@@ -35,13 +35,13 @@ public class Robot extends TimedRobot {
 
     m_robotDrive = new MecanumDrive(frontLeft, rearLeft, frontRight, rearRight);
 
-    m_stick = new Joystick(kJoystickChannel);
   }
 
   @Override
   public void teleopPeriodic() {
     // Use the joystick X axis for forward movement, Y axis for lateral
     // movement, and Z axis for rotation.
-    m_robotDrive.driveCartesian(-m_stick.getY(), -m_stick.getX(), -m_stick.getZ());
+    m_robotDrive.driveCartesian(-m_driverController.getLeftY(), -m_driverController.getLeftX(), 0);
+
   }
 }
